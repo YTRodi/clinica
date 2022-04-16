90fc58a843b52500145d96b0ad1621979120dc4c
YTRodi <yagorodi@gmail.com>
2022-04-16 14:43:01 -0300

    feat(auth): unauthenticated and authenticated module and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  // TODO: pending implementation
+  // { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  // {
+  //   path: 'welcome',
+  //   loadChildren: () =>
+  //     import('@public/public.module').then(({ PublicModule }) => PublicModule),
+  // },
+  // { path: '', pathMatch: 'full', redirectTo: 'auth/login' },
   {
-    path: 'welcome',
+    path: '',
     loadChildren: () =>
-      import('@public/public.module').then(({ PublicModule }) => PublicModule),
+      import('@auth/auth.module').then(({ AuthModule }) => AuthModule),
   },
 ];
 
---
+++ src/app/auth/authenticated/authenticated-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MainComponent } from './pages/main/main.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthenticatedRoutingModule {}
